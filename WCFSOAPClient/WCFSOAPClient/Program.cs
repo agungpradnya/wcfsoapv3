ceeba2fb4e1e1098f1e0cc3f303f1cc8b7593d5e
agungpradnya <gbagung.pradnyaandikaputra@mitrais.com>
2016-08-31 11:17:06 +0800

    Add WCF Client

---
+++ WCFSOAPClient/WCFSOAPClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WCFSOAPClient.ServiceReference1;
+
+namespace WCFSOAPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DistributorServiceClient client = new DistributorServiceClient();
+            var distributors = client.GetAllDistributors();
+            foreach (var d in distributors)
+            {
+                Console.WriteLine(d.BODS_Id);
+                Console.WriteLine(d.BODS_FullName);                
+                Console.WriteLine(d.BODS_Status);
+            }
+            Console.ReadLine();
+        }
+    }
+}
